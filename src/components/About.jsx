54cc3e3c8b8a3b57348fc889aa700c58e15431e1
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-12-21 17:40:14 +0700

    Routing

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = (props) => {
+  setTimeout(() => {
+    props.history.push("/about");
+  }, 2000);
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem Ipsum dolor sit about</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+
+const Navbar = (props) => {
+  setTimeout(() => {
+    props.history.push("/about");
+  }, 2000);
+  return (
+    <div className="navbar-fixed">
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#!" className="brand-logo">REACT ROUTE</a>
+          <ul className="right hide-on-med-and-down">
+            <li><Link to="/">Home</Link></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default withRouter(Navbar);
