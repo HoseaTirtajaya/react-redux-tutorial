d0cc9299c21fd114f44923b3828a67d26cf2bf20
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-12-22 12:15:13 +0700

    update axios with componentdidmount

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>Lorem Ipsum</p>
-    </div>
-  );
+class Home extends Component {
+  state = {
+    posts: [ ],
+    user: [ ]
+  }
+  async componentDidMount() {
+    let data = await axios({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/posts"
+    });
+    let userdata = await axios({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/users"
+    });
+
+    console.log(userdata);
+    console.log(data.data.slice(0, 50))
+    this.setState({
+      posts: data.data.slice(0, 50),
+      user: userdata.data
+    });
+  }
+  render() {
+    const { posts, user } = this.state;
+    const postList = (posts.length && user.length) ? (
+      posts.map(item => {
+          return (
+            <div className="post card" key={item.id}>
+              <div className="card-content">
+                <span className="card-title">{item.title}</span>
+                <p>{item.body}</p>
+              </div>
+            </div>
+          )
+        })
+    ) : (
+      <div className="center">No Posts Yet</div>
+    )
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
 }
 
 export default Home;
