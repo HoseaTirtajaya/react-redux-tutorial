cb42405a5d1829886eaef44856e55707889296fa
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-12-23 13:34:53 +0700

    REACT REDUX DONE!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,13 @@
 import React, {Component} from "react";
-import axios from "axios";
 import {Link} from "react-router-dom";
-import Logo from "../logo.png"
+import { connect } from "react-redux"
+// import Logo from "../logo.png"
 import "../index.css"
 
 class Home extends Component {
-  state = {
-    posts: [ ],
-    user: [ ]
-  }
-  async componentDidMount() {
-    let data = await axios({
-      method: "GET",
-      url: "https://jsonplaceholder.typicode.com/posts"
-    });
-    let userdata = await axios({
-      method: "GET",
-      url: "https://jsonplaceholder.typicode.com/users"
-    });
-
-    this.setState({
-      posts: data.data.slice(0, 50),
-      user: userdata.data
-    });
-  }
   render() {
-    const { posts, user } = this.state;
-    const postList = (posts.length && user.length) ? (
+    const { posts } = this.props;
+    const postList = (posts.length) ? (
       posts.map(item => {
           return (
             <div className="post card black-text" key={item.id}>
@@ -52,4 +33,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+      ...state
+    }
+}
+
+export default connect(mapStateToProps)(Home);
